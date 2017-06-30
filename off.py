9c8849fe1acdb669c0b06cc49c9120594af797fd
PragnyaKondrakunta <pragnyakondrakunta@gmail.com>
2017-06-30 15:11:22 +0530

    commit from pc

---
+++ off.py
@@ -0,0 +1,5 @@
+from pubnub import Pubnub
+
+pubnub = Pubnub(publish_key="pub-c-e7f7f579-146d-48f8-adec-3f38ce14b0e9", subscribe_key="sub-c-e240c026-5c1a-11e7-8944-0619f8945a4f")
+
+print (pubnub.publish(channel='my_channel', message="off"))
