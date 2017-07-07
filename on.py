00e5dad07ba8090af3eef6ffae1513e475e02279
PragnyaKondrakunta <PragnyaKondrakunta@users.noreply.github.com>
2017-07-07 10:54:17 +0530

    generalization of the keys

--- on.py
+++ on.py
@@ -1,5 +1,5 @@
 from pubnub import Pubnub
 
-pubnub = Pubnub(publish_key="pub-c-e7f7f579-146d-48f8-adec-3f38ce14b0e9", subscribe_key="sub-c-e240c026-5c1a-11e7-8944-0619f8945a4f")
+pubnub = Pubnub(publish_key="publish_key", subscribe_key="subscribe_key")
 
 print (pubnub.publish(channel='my_channel', message="on"))
