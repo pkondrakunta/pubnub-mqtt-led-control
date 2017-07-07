e2c2d7fd6041b7af9c78dd704278676c2512ee66
PragnyaKondrakunta <PragnyaKondrakunta@users.noreply.github.com>
2017-07-07 10:52:53 +0530

    Update off.py

--- off.py
+++ off.py
@@ -1,5 +1,5 @@
 from pubnub import Pubnub
 
-pubnub = Pubnub(publish_key="publish_key", subscribe_key="subcribe_key")
+pubnub = Pubnub(publish_key="publish_key", subscribe_key="subscribe_key")
 
 print (pubnub.publish(channel='my_channel', message="off"))
